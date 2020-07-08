181d5273c22772a7a59979543f91fa7cd444adad
Naveen Yalla <naveenyalla22@gmail.com>
2020-07-08 21:38:14 +0530

    Update header.js

--- src/components/header.js
+++ src/components/header.js
@@ -1,8 +1,29 @@
 import React from "react"
+import { ThemeToggler } from 'gatsby-plugin-dark-mode'
+
+class MyComponent extends React.Component {
+  render() {
+    return (
+      <ThemeToggler>
+        {({ theme, toggleTheme }) => (
+          <label>
+            <input
+              type="checkbox"
+              onChange={e => toggleTheme(e.target.checked ? 'dark' : 'light')}
+              checked={theme === 'dark'}
+            />{' '}
+            Dark mode
+          </label>
+        )}
+      </ThemeToggler>
+    )
+  }
+}
 
 const Header = ({children}) => (
   <header className="site-header">
     {children}
+  <MyComponent/>
   </header>
 )
 
