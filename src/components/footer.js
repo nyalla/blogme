9e59d6a8b009c361dc16f19e7dce08aaab6188d0
Naveen Yalla <naveenyalla22@gmail.com>
2020-07-14 18:42:32 +0530

    updated footer
    updated footer

--- src/components/footer.js
+++ src/components/footer.js
@@ -5,7 +5,7 @@ import { RiHeart2Line } from "react-icons/ri";
 const Footer = () => (
   <footer className="site-footer">
     <div className="container">
-      <p>Made with <span className="icon -love"><RiHeart2Line/></span> by <Link to="/">nyalla.me</Link></p>
+     <p>Made with <span className="icon -love"><RiHeart2Line/></span> by <a href="https://github.com/nyalla" target="_blank">@nyalla</a></p>
     </div>
   </footer>
 )
